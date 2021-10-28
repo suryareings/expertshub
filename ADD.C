8069cecd31a9b395d017ca2ece664bdd8b99be9e
suryareings <93313962+suryareings@users.noreply.github.com>
2021-10-28 14:03:36 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+   int input1,input2,output1;
+   clrscr();
+   printf("enter the input =");
+   scanf("%d%d",&input1,&input2);
+  // printf("enter the input2=");
+  // scanf("%d",&input2);
+   output1=input1+input2;
+   printf("sum of values are%d",output1);
+   getch();
+}
+
+
+
